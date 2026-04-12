f673369f505d256e5b6e9ff5107a94fc183caeca
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-04-12 22:32:48 +0530

    feat: send unit for jvm and ios

--- composeApp/src/iosMain/kotlin/com/iam2kabhishek/notey/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/iam2kabhishek/notey/MainViewController.kt
@@ -2,4 +2,4 @@ package com.iam2kabhishek.notey
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App(Unit) }
