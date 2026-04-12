e28eeb18f2a9091c61d5336e8617734980133b35
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-04-12 22:31:52 +0530

    feat: setup manual di

---
+++ composeApp/src/commonMain/kotlin/com/iam2kabhishek/notey/di/AppContainer.kt
@@ -0,0 +1,13 @@
+package com.iam2kabhishek.notey.di
+
+import com.iam2kabhishek.notey.data.local.buildDatabase
+import com.iam2kabhishek.notey.data.local.getDatabaseBuilder
+import com.iam2kabhishek.notey.data.repository.NoteRepository
+import com.iam2kabhishek.notey.ui.notes.NotesViewModel
+
+class AppContainer(platformContext: Any) {
+    private val database = buildDatabase(getDatabaseBuilder(platformContext))
+    private val noteRepository = NoteRepository(database.noteDao())
+
+    val notesViewModel: NotesViewModel = NotesViewModel(noteRepository)
+}
