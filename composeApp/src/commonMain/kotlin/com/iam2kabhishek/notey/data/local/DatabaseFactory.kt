554f16ed69ce3e877fc9751e7d3dadf953f05d18
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-04-12 22:32:58 +0530

    fix: database factory

--- composeApp/src/commonMain/kotlin/com/iam2kabhishek/notey/data/local/DatabaseFactory.kt
+++ composeApp/src/commonMain/kotlin/com/iam2kabhishek/notey/data/local/DatabaseFactory.kt
@@ -3,13 +3,12 @@ package com.iam2kabhishek.notey.data.local
 import androidx.room.RoomDatabase
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 
 expect fun getDatabaseBuilder(context: Any): RoomDatabase.Builder<AppDatabase>
 
 fun buildDatabase(builder: RoomDatabase.Builder<AppDatabase>): AppDatabase {
     return builder
         .setDriver(BundledSQLiteDriver())
-        .setQueryCoroutineContext(Dispatchers.IO)
+        .setQueryCoroutineContext(Dispatchers.Default)
         .build()
 }
