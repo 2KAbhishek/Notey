0c06ef304085582e98d04100163475ec958b4779
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-04-23 15:47:40 +0530

    fix: build app for iOS

--- composeApp/src/commonMain/kotlin/com/iam2kabhishek/notey/data/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/iam2kabhishek/notey/data/AppDatabase.kt
@@ -2,6 +2,8 @@ package com.iam2kabhishek.notey.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.ConstructedBy
+import androidx.room.RoomDatabaseConstructor
 import com.iam2kabhishek.notey.data.notes.NoteDao
 import com.iam2kabhishek.notey.data.notes.NoteEntity
 
@@ -9,6 +11,12 @@ import com.iam2kabhishek.notey.data.notes.NoteEntity
     entities = [NoteEntity::class],
     version = 1
 )
+@ConstructedBy(AppDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
+}
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
+    override fun initialize(): AppDatabase
 }
